31afbf58309cc7583329fc88aaf632ec7a3bb60b
rzapriono <apriono.reza@gmail.com>
2023-02-10 11:25:24 +0700

    Sudah selesai mengerjakan lab 0

--- java/src/nilai.java
+++ java/src/nilai.java
@@ -5,7 +5,7 @@ public class nilai {
         System.out.print("Masukkan Nama Mahasiswa: ");
         Scanner input_nama = new Scanner(System.in);
         String nama = input_nama.next();
-        //tes
+        
         System.out.print("Masukkan Nilai Asli: ");
         Scanner input_nilai_asli = new Scanner(System.in);
         double nilai_asli = input_nilai_asli.nextDouble();
@@ -19,14 +19,14 @@ public class nilai {
         if (durasi < 60.0){
             nilai_akhir = 1.2 * nilai_asli;
         }
-        else if (durasi <= 70 & durasi >= 60.0){
+        else if (durasi <= 70 && durasi >= 60.0){
             nilai_akhir = nilai_asli;
             
         }
-        else if (durasi < 90.0 & durasi > 70){
+        else if (durasi < 90.0 && durasi > 70){
             nilai_akhir = 0.75 * nilai_asli;
         }
-        else if (durasi <= 100 & durasi >= 90){
+        else if (durasi <= 100 && durasi >= 90){
             nilai_akhir = 0.5 * nilai_asli;
         }
         else
